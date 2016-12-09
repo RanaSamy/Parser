daaf53d904ee7c1057c014c5910fea9c5038c900
DoaaOsama <doaa_kandeel@hotmail.com>
2016-12-09 23:12:21 +0200

    Scanner done

--- RecursiveDescentParser/src/parser/Parser.java
+++ RecursiveDescentParser/src/parser/Parser.java
@@ -17,7 +17,17 @@ public class Parser {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        scanner F = new scanner("if dalia = 35 + 98; then hi");
+        scanner F = new scanner("{ Sample program in TINY language – computes factorial\n" +
+"    }\n" +
+"     read x;   {input an integer }\n" +
+"     if  0 < x   then     {  don’t compute if x <= 0 }\n" +
+"        fact  := 1;\n" +
+"        repeat \n" +
+"           fact  := fact *  x;\n" +
+"            x  := x  -  1\n" +
+"        until  x  =  0;\n" +
+"        write  fact   {  output  factorial of x }\n" +
+"     end  ");
         F.get_token();
         
     }
