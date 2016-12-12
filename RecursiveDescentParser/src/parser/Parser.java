84cb4dec92ab7a68032addc3569592bb82aab0e3
DoaaOsama <doaa_kandeel@hotmail.com>
2016-12-12 18:17:06 +0200

    updates read error error

--- RecursiveDescentParser/src/parser/Parser.java
+++ RecursiveDescentParser/src/parser/Parser.java
@@ -4,22 +4,30 @@
  * and open the template in the editor.
  */
 package parser;
-
-import java.util.Vector;
-import static parser.Parser.stmtTypes.*;
-
-
-
+import java.util.*;
 /**
  *
- * @author Doaa,Rana,Dalia
+ * @author Doaa
  */
-public class Parser {
-  private scanner scanner;
-  public Parser(scanner scanner) {  
-    this.scanner = scanner;  
-} 
-  /* This program illustrates recursive descent parsing using a 
+public class Parser {   /**
+     * @param args the command line arguments
+     */ 
+    private scanner s;
+    public Parser(scanner scanner) {  
+      s = scanner;
+    } 
+    
+    
+    Vector <String> temp= new Vector<String>();
+    Vector initv(){
+        temp = s.get_token();
+        return temp;
+    }
+    
+    
+    
+    
+    /* This program illustrates recursive descent parsing using a 
    pure procedural approach. 
  
    The grammar: 
@@ -29,218 +37,223 @@ public class Parser {
    term      = factor { ( "*" | "/" ) factor } 
    factor    = number | "(" expression ")" 
      
-*/  
- 
- 
-  
+    //if exp then stmt-eeq [else ]
+  //stmt{; stmt}
+    
+    
+    exp  term  { addop term }
+      addop  + | -
+      term  factor  { mulop factor }
+      mulop  *
+      factor  (exp)  |  number
+
+   */  
+    
+    
   int i=0;
   //String token= getstring(j);
   String token;
-  public enum stmtTypes {IF, REPEAT, ASSIGN, READ, WRITE, ERROR};
-/*  public String getstring(int i)
-  {  
-      String t = null;
-       Vector<String> vec=scanner.get_token();
-      if(i >= 0 || i< vec.size())
-      {
-           t=vec.get(i);  
-      }
-      return t;
+  
+  //public enum stmtTypes {IF, REPEAT, ASSIGN, READ, WRITE, ERROR}; 
+  
+ //ArrayList<String> test =new ArrayList<String>(Arrays.asList("READ","IDENTIFIER",";","IF","NUMBER","=","IDENTIFIER","THEN","IDENTIFIER",":=","NUMBER",";","END" ));
+ String init(){
+     initv();
+ token= temp.get(i);
+ return token;
+ }
+ 
+ void error(){System.out.println("Error");}
+         
+         
+ void match(String s)
+ {
+     if(s.equals(token))
+     {
+        i++;
+        token = temp.get(i);
+     }
+     else 
+         error();
+ }
+  
+  void factor(){
+  if(token.equals("("))
+  {
+      match("(");
+      expression();
+      match(")");
   }
-*/
-stmtTypes getStmtType(String s)
-{
-    s=token;
-	if ("IF".equals(s)) return IF;
-	if ("IDENTIFIER".equals(s)) return ASSIGN;
-	if ("READ".equals(s)) return READ;
-	if ("WRITE".equals(s)) return WRITE;
-	if ("REPEAT".equals(s)) return REPEAT;
-	else return ERROR;
-}
-void match(String s) 
-{
-     //Vector<String> vec=scanner.get_token();
-	if (token.equals(s)) 
-	{ 
-          /*  for(int i =0; i < vec.size();i++)
-            {
-                token=vec.get(i);
-                //System.out.println(token);
-            }  */
-		//System.out.println("doesn't match");
-             Vector<String> vec=scanner.get_token();
-             if(i< 12)
-             {
-                 i++;  
-                 token=vec.get(i);
-             }
-          
-            
-            
-	} 
-        else{
-            
-            System.out.println("doesn't match");
-        }
-        
-	
-}
+  else if (token.equals("NUMBER"))
+  {
+      match("NUMBER");
+  }
+  else error();
+  }
+  
+  void expression()
+  {
+      simple_exp();
+      if(token.equals("<")|| token.equals("="))
+      {
+          compop();
+          simple_exp();
+      }
+  }
+  
+  void simple_exp()
+  {
+     term();
+     while (token.equals("+")|| token.equals("-"))
+     {
+         addop();
+         term();
+     }
+  }
+     
+  void term()
+  {
+      factor();
+      while(token.equals("*")||token.equals("/"))
+      {
+          mulop();
+          term();
+      }
+      
+  }
+  
+  void addop()
+  {
+      if(token.equals("+")||token.equals("-"))
+      {
+          match(token);
+      }
+      else error();
+  }
+  
+  void mulop()
+  {
+      if(token.equals("*")||token.equals("/"))
+      {
+          match(token);
+      }
+      else error();
+  }
+  
+  void compop()
+  {
+      if(token.equals("<")||token.equals("="))
+      {
+          match(token);
+      }
+      else error();
+  }
+  
   void stmt_seq()
-{
-	stmt();
-	while (";".equals(token)) 
-	{
-		match(";");
-		stmt();
-	}
-}
-  void stmt() 
-{
-     Vector<String> vec=scanner.get_token();
-          token=vec.get(i);
-          
-	switch (getStmtType(token))
-	{
-	case IF:
-		if_stmt();
-                break;
-	case REPEAT:
-		repeat_stmt();
-                break;
-	case ASSIGN:
-		assign_stmt();
-                break;
-	case READ: 
-		read_stmt(); 
-                break;
-	case WRITE:
-		write_stmt();
-                break;
-	case ERROR:
-		System.out.println("Statement not found"); 
-                break;
-	default:
-		break;
-	}
-       // i++;
-}
-  void if_stmt() 
-{
-	match("IF");
-	exp();
-	match("THEN");
-	stmt_seq();
-	if ("ELSE".equals(token)) stmt_seq();
-	match("END");
-	System.out.println( "- if statement found");
-}
-  void repeat_stmt() 
-{
-	match("REPEAT");
-	stmt_seq();
-	match("UNTIL");
-	exp();
-	System.out.println("- repeat found");
-}
-  void assign_stmt() 
-{
-	match("IDENTIFIER");
-	match(":=");
-	exp();
-	System.out.println("- assignment found");
-}
-void read_stmt() 
-{
-	match("READ");
-	match("IDENTIFIER");
-	System.out.println("- read found");
-}
-void write_stmt() 
-{
-	match("WRITE");
-	exp();
-	System.out.println("- write found" );
-}
-void exp() 
-{
-	simple_exp();
-	if ("<".equals(token) || "=".equals(token)) 
-	{
-		comparison_op();
-		simple_exp();
-	}
-}
-void comparison_op() 
-{
-	if ("<".equals(token)) match("<");
-	if ("=".equals(token)) match("=");
-}
-void simple_exp() 
-{
-	term();
-	while ("+".equals(token) || "-".equals(token)) {
-		addop();
-		term();
-	}
-}
-void addop() 
-{
-	if ("+".equals(token)) match("+");
-	if ("-".equals(token)) match("-");
-}
-void term() 
-{
-	factor();
-	while ("*".equals(token) || "/".equals(token)) 
-	{
-		mulop();
-		factor();
-	}
-}
-void mulop() 
-{
-	if ("*".equals(token)) match("*");
-	if ("/".equals(token)) match("/");
-}
-void factor() 
-{
-	if ("(".equals(token)) 
-	{ 
-		match("(");
-		exp();
-		match(")");
-	}
-	else if ("NUMBER".equals(token)) 
-	{
-		match("NUMBER");
-	}
-	else if ("IDENTIFIER".equals(token)) 
-	{
-		match("IDENTIFIER");
-	}
-}
-
+  {
+      stmt();
+      while(token.equals(";"))
+      {
+          match(";");
+          stmt();
+      }
+  }
+  
+  void stmt()
+  {
+      if(token.equals("IF"))
+      {
+          if_stmt();
+      }
+      else if(token.equals("REPEAT"))
+      {
+          repeat_stmt();
+      }
+      else if (token.equals("ASSIGN"))
+      {
+          assign_stmt();
+      }
+      else if (token.equals("READ"))
+      {
+          read_stmt();
+      }
+      else if (token.equals("WRITE"))
+      {
+          write_stmt();
+      }
+      
+  }
+  
+  void if_stmt()
+  {
+      match("IF");
+      expression();
+      match("THEN");
+      stmt_seq();
+      if(token.equals("ELSE"))
+      {
+          stmt_seq();
+      }
+      match("END");
+      System.out.println("IF found");
+  }
+  
+  void repeat_stmt()
+  {
+      match("REPEAT");
+      stmt_seq();
+      match("UNTIL");
+      expression();
+      System.out.println("repeat found");
+      
+  }
+  
+  void assign_stmt()
+  {
+      match("IDENTIFIER");
+      match(":=");
+      expression();
+      System.out.println("assign found");
+  }
+  
+  void read_stmt()
+  {
+      match("READ");
+      match("IDENTIFIER");
+      System.out.println("read found");
+  }
+  
+  void write_stmt()
+  {
+      match("WRITE");
+      expression();
+      System.out.println("write found");
+  }
+  
 void program()
 {
-	stmt_seq();
-	System.out.println("-- Program found" );
-        
+   stmt_seq(); 
 }
 
-    /**
-     * @param args the command line arguments
-     */
+
+
+  
     public static void main(String[] args) {
-        // TODO code application logic here
         
-        scanner F = new scanner(
-         "if  0 <= x then x := 1 " + "read x;");
-       // F.get_token();
-        System.out.println("..........parser output .........");
-        Parser p = new Parser(F);
-        p.program();
-        
-    }
+          // TODO code application logic here
+          
+// Vector <String> temp= new Vector<String>();
+        scanner F = new scanner("{ Sample program in TINY language – computes factorial} read x;   {input an integer } if  0 < x   then     {  don’t compute if x <= 0 } fact  := 1; repeat   fact  := fact *  x; x  := x  -  1; until  x  =  0; write  fact   {  output  factorial of x } end   ");
+         F.get_token();
+//         temp= F.get_token();
+         
+//     for(String s: temp){System.out.println(s); }
+//      
+         Parser P = new Parser(F);
+            P.init();
+            P.program();
+
+         
+   }
     
 }
